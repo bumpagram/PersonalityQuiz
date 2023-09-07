c01e77955d9d326dc40e33f558b426d324205b22
bumpagram <invoke@kxdigital.ru>
2023-09-07 16:52:27 +0300

    part 1 and 2 ready

--- PersonalityQuiz/IntroVC.swift
+++ PersonalityQuiz/IntroVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IntroVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ QuestionVC.swift
@@ -0,0 +1,29 @@
+//
+//  QuestionVC.swift
+//  PersonalityQuiz
+//
+//  Created by Denis Azarkov on 9/7/23.
+//
+
+import UIKit
+
+class QuestionVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
