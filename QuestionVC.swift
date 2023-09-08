849ae8ce2c335bee7817ab20128512f883a9ba5d
bumpagram <invoke@kxdigital.ru>
2023-09-08 13:42:49 +0300

    singleStack закончил, начинаю multiStack логику делать

--- QuestionVC.swift
+++ QuestionVC.swift
@@ -29,14 +29,32 @@ class QuestionVC: UIViewController {
     @IBOutlet var rangedLabel2: UILabel!
     
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
         // Do any additional setup after loading the view.
     }
     
-    // var answers: [Answer] = []
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[indexQuestion].answers //сняли массив в массиве, передали в константу, обратимся по индексу ответов
+        
+        switch sender {
+        case singleButton1 : answersChosen.append(currentAnswers[0])
+        case singleButton2 : answersChosen.append(currentAnswers[1])
+        case singleButton3: answersChosen.append(currentAnswers[2])
+        case singleButton4: answersChosen.append(currentAnswers[3])
+        default: break
+        }
+        nextQuestion()
+    }
+    
+    
+    
+    
+    
+    
+    var answersChosen = [Answer]() // коллекция ответов от юзера
+    //var answers: [Answer] = []
     var indexQuestion = 0
 
     var questions: [Question] =  [
@@ -114,7 +132,9 @@ class QuestionVC: UIViewController {
         rangedLabel1.text = answers.first?.text // проперти коллекции, обращение к первому элементу
         rangedLabel2.text = answers.last?.text // аналогично
     }
-
+    func nextQuestion() {
+        // код будет позже 
+    }
     
     
     
